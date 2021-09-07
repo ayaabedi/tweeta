f7df40fb64a2622c560519096e71be8fa7e61747
ayaabedi <alabedi.aya@gmail.com>
2021-09-07 11:37:31 -0500

    add user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
+const bcrypt = require('bcrypt')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* register users listing. */
+router.post('/register', function(req, res, next) {
+  // take username, password
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({
+      error: 'please include username and password'
+    })
+    return
+  }
+
+  // check if username is taken
+  db.User.findOne({ 
+    where: {
+      username: req.body.username
+    }
+  })
+    .then((user) => {
+      if (user) {
+        res.status(400).json({
+          error: 'username already in use'
+        })
+        return
+      }
+
+      // hash password
+      bcrypt.hash(req.body.password, 10)
+        .then((hash) => {
+          // store in database
+          db.User.create({
+            username: req.body.username,
+            password: hash
+          })
+            .then((user) => {
+              // respond with success/error
+              res.status(201).json({
+                success: 'User created'
+              })
+            })
+        })
+    })
 });
 
 module.exports = router;
